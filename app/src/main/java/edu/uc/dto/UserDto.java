a037157dc5790e501b671ab9b116d2693a81103d
Matthew Corbett <corbetmw@mail.uc.edu>
2015-06-16 10:27:33 -0400

    Commiting the preliminary data structure for the UserDto as well as modified res/values/styles.xml in order to generate preview.

---
+++ app/src/main/java/edu/uc/dto/UserDto.java
@@ -0,0 +1,71 @@
+package edu.uc.dto;
+
+/**
+ * Created by matthewcorbett on 6/14/15.
+ */
+public class UserDto {
+
+    public int userID;
+    public int photoID;
+    public int distance;
+    public double latitude;
+    public double longitude;
+    public String userName;
+    public String foodPreference;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getPhotoID() {
+        return photoID;
+    }
+
+    public void setPhotoID(int photoID) {
+        this.photoID = photoID;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFoodPreference() {
+        return foodPreference;
+    }
+
+    public void setFoodPreference(String foodPreference) {
+        this.foodPreference = foodPreference;
+    }
+}
