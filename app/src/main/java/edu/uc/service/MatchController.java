7f907d14f5d0a1b9d4e5bf6e648e09d834aaeec4
Tonald <twaldeck13@gmail.com>
2015-06-16 13:27:01 -0400

    Added the MatchController interface

---
+++ app/src/main/java/edu/uc/service/MatchController.java
@@ -0,0 +1,19 @@
+package edu.uc.service;
+
+/**
+ * Created by Anthony on 6/16/2015.
+ */
+public interface MatchController {
+
+    /**
+     * fetches a match for a person
+     * @return boolean if there is a match
+     */
+    boolean isThereMatch();
+
+
+    /**
+     * actually gets the match
+     */
+    void fetchMatch();
+}
