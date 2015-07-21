ebabc3d6e200483c7e554387bfa415aa6d2efd25
tripleao <tripleao@mail.uc.edu>
2015-07-21 07:36:39 -0400

    MainActivity UI Updates with collapsable scrolling on versions 14+

---
+++ app/src/main/java/edu/uc/dto/PreviousDates.java
@@ -0,0 +1,15 @@
+package edu.uc.dto;
+
+/**
+ * Created by Suleiman on 14-04-2015.
+ */
+public class PreviousDates {
+    public String name;
+
+    public static final String[] data = {"Sam","Katie","Lauren","Janice","Kim"};
+
+    PreviousDates(String name){
+        this.name=name;
+    }
+}
+
--- app/src/main/java/edu/uc/ui/activities/MainActivity.java
+++ app/src/main/java/edu/uc/ui/activities/MainActivity.java
@@ -1,24 +1,98 @@
 package edu.uc.ui.activities;
 
-import android.support.design.widget.Snackbar;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.uc.R;
+import edu.uc.dto.PreviousDates;
+import edu.uc.ui.adapters.SimpleRecyclerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbar;
+    RecyclerView recyclerView;
+    int toolbarScrimColor = R.attr.colorPrimary;
+    int titleScrimColor = R.color.white;
+    SimpleRecyclerAdapter simpleRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.anim_toolbar);
+        setSupportActionBar(toolbar);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.header);
+
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Hello Snackbar", Snackbar.LENGTH_LONG).show();
+            public void onGenerated(Palette palette) {
+                applyPalette(palette);
             }
         });
+
+        collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("Dates");
+        collapsingToolbar.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+
+        recyclerView = (RecyclerView) findViewById(R.id.scrollableview);
+
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        List<String> listData = new ArrayList<String>();
+        int ct = 0;
+        for (int i = 0; i < PreviousDates.data.length; i++) {
+            listData.add(PreviousDates.data[ct]);
+            ct++;
+            if (ct == PreviousDates.data.length) {
+                ct = 0;
+            }
+        }
+
+        if (simpleRecyclerAdapter == null) {
+            simpleRecyclerAdapter = new SimpleRecyclerAdapter(listData);
+            recyclerView.setAdapter(simpleRecyclerAdapter);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void applyPalette(Palette palette) {
+        int primaryDark = getResources().getColor(R.color.primary_dark);
+        int primary = getResources().getColor(R.color.primary);
+        collapsingToolbar.setContentScrimColor(palette.getMutedColor(primary));
+        collapsingToolbar.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
     }
 }
