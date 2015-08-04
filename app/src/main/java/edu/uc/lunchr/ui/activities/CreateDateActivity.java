75d9636605cacba0ee539ada2145d8fe5dc25fec
Matthew Corbett <corbetmw@mail.uc.edu>
2015-08-04 19:22:29 -0400

    added date and time picking functionality for dates

--- app/src/main/java/edu/uc/lunchr/ui/activities/CreateDateActivity.java
+++ app/src/main/java/edu/uc/lunchr/ui/activities/CreateDateActivity.java
@@ -1,5 +1,7 @@
 package edu.uc.lunchr.ui.activities;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,20 +10,36 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
+import java.util.Calendar;
+
 import edu.uc.lunchr.R;
 
 public class CreateDateActivity extends AppCompatActivity {
+
+    Button datePickerBtn;
+    Button timePickerBtn;
+    Button locationPickerBtn;
+
+    String datePickerString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_date);
+
+        datePickerBtn = (Button) findViewById(R.id.pick_date_button);
+        timePickerBtn = (Button) findViewById(R.id.pick_time_button);
+        locationPickerBtn = (Button) findViewById(R.id.pick_a_place_button);
+
     }
 
     public void onPickPlaceClick(View v) {
@@ -36,6 +54,27 @@ public class CreateDateActivity extends AppCompatActivity {
         } catch (GooglePlayServicesNotAvailableException e) {
             Log.i("WTF", "IS HAPPENING NOT AVAIL?");
         }
+    }
+
+    public void OnPickDateClick(View view) {
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dpd = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        datePickerString = dayOfMonth + "-"
+                                + (monthOfYear + 1) + "-" + year;
+
+                        datePickerBtn.setText(datePickerString);
+                    }
+                }, mYear, mMonth, mDay);
+        dpd.show();
     }
 
     @Override
@@ -66,7 +105,20 @@ public class CreateDateActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void OnPickDateTimeClick(View view) {
+    public void OnPickTimeClick(View view) {
+        final Calendar c = Calendar.getInstance();
+        final int mHour = c.get(Calendar.HOUR_OF_DAY);
+        final int mMinute = c.get(Calendar.MINUTE);
 
+        TimePickerDialog tpd = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+                        timePickerBtn.setText(hourOfDay + ":" + minute);
+                    }
+                }, mHour, mMinute, false);
+        tpd.show();
     }
 }
